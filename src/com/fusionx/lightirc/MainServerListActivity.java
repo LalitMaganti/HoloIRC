6b63a41cd051e7162ed7a1721bb8519a40a9101f
Lalit Maganti <lalitmaganti@gmail.com>
2013-02-28 22:15:17 +0000

    LightIRC: multiple updates
    * Start including .project file
    * Change Activity to ListActivity to handle inital startup
    * Add test values to display on ListActivity

--- src/com/fusionx/lightirc/MainServerListActivity.java
+++ src/com/fusionx/lightirc/MainServerListActivity.java
@@ -1,20 +1,26 @@
 package com.fusionx.lightirc;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class MainServerListActivity extends Activity {
+public class MainServerListActivity extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_server);
+	    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+	        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+	        "Linux", "OS/2" };
+	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+	        android.R.layout.simple_list_item_1, values);
+	    setListAdapter(adapter);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_server_list, menu);
 		return true;
 	}
