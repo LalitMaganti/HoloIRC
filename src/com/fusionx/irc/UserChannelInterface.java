4db965fe636a43212c8f73ef65de9ee6119261c1
Lalit Maganti <lalitmaganti@gmail.com>
2013-08-17 15:24:31 +0100

    holoirc: add copyrights

--- src/com/fusionx/irc/UserChannelInterface.java
+++ src/com/fusionx/irc/UserChannelInterface.java
@@ -1,3 +1,24 @@
+/*
+    HoloIRC - an IRC client for Android
+
+    Copyright 2013 Lalit Maganti
+
+    This file is part of HoloIRC.
+
+    HoloIRC is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HoloIRC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HoloIRC. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.fusionx.irc;
 
 import android.content.Context;
--- src/com/fusionx/irc/enums/CoreEventType.java
+++
@@ -1,5 +0,0 @@
-package com.fusionx.irc.enums;
-
-public enum CoreEventType {
-    Ping,
-}
