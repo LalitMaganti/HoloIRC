97c68bdc01d1ece07bbfbc428c389c8608223d86
Lalit Maganti <lalitmaganti@gmail.com>
2013-08-22 10:29:53 +0100

    holoirc: fix crash in ServerChannelSettings
    * ClassCastException - it was changed to an edittextpreference a few commits ago

--- src/com/fusionx/lightirc/fragments/settings/ServerChannelSettingsFragment.java
+++ src/com/fusionx/lightirc/fragments/settings/ServerChannelSettingsFragment.java
@@ -22,6 +22,7 @@
 package com.fusionx.lightirc.fragments.settings;
 
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
@@ -29,18 +30,19 @@ import com.fusionx.lightirc.R;
 import com.fusionx.lightirc.misc.PreferenceKeys;
 import com.fusionx.lightlibrary.ui.NonEmptyEditTextPreference;
 
-public class ServerChannelSettingsFragment extends PreferenceFragment
-        implements Preference.OnPreferenceChangeListener {
+public class ServerChannelSettingsFragment extends PreferenceFragment implements Preference
+        .OnPreferenceChangeListener {
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.server_channel_settings_fragment);
 
-        final NonEmptyEditTextPreference partReason = (NonEmptyEditTextPreference) getPreferenceScreen()
+        final EditTextPreference partReason = (EditTextPreference) getPreferenceScreen()
                 .findPreference(PreferenceKeys.PartReason);
         partReason.setOnPreferenceChangeListener(this);
         partReason.setSummary(partReason.getText());
-        final NonEmptyEditTextPreference quitReason = (NonEmptyEditTextPreference) getPreferenceScreen()
+
+        final EditTextPreference quitReason = (EditTextPreference) getPreferenceScreen()
                 .findPreference(PreferenceKeys.QuitReason);
         quitReason.setOnPreferenceChangeListener(this);
         quitReason.setSummary(quitReason.getText());
@@ -48,7 +50,7 @@ public class ServerChannelSettingsFragment extends PreferenceFragment
 
     @Override
     public boolean onPreferenceChange(final Preference preference, final Object o) {
-        final NonEmptyEditTextPreference editTextPreference = (NonEmptyEditTextPreference) preference;
+        final EditTextPreference editTextPreference = (EditTextPreference) preference;
         editTextPreference.setSummary((CharSequence) o);
         return true;
     }
