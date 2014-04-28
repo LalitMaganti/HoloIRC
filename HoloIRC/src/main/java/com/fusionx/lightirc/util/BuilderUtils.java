4dcb2931bfb6552d9afa81128856aa6330fadf00
tehsu <tehsu@users.noreply.github.com>
2014-04-28 06:16:23 -0500

    Readd Snyde IRC Network

--- HoloIRC/src/main/java/com/fusionx/lightirc/util/BuilderUtils.java
+++ HoloIRC/src/main/java/com/fusionx/lightirc/util/BuilderUtils.java
@@ -23,7 +23,12 @@ public class BuilderUtils {
         tmwirc.setTitle("Techman's World IRC").setUrl("irc.techmansworld.com").setPort(6667).setSsl(false);
         tmwirc.setNickStorage(new NickStorage("HoloIRCUser", "", ""));
         tmwirc.setRealName("HoloIRCUser").setNickChangeable(true).setServerUserName("holoirc");
+        
+        final ServerConfiguration.Builder snyde = new ServerConfiguration.Builder();
+        snyde.setTitle("Snyde").setUrl("irc.snyde.net").setPort(6667).setSsl(false);
+        snyde.setNickStorage(new NickStorage("HoloIRCUser", "", ""));
+        snyde.setRealName("HoloIRCUser").setNickChangeable(true).setServerUserName("holoirc");
 
-        return Arrays.asList(freenode, snoonet, tmwirc);
+        return Arrays.asList(freenode, snoonet, tmwirc, snyde);
     }
 }
