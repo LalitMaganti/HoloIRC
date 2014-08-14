8bf7c28a55f802da540c97bbe92c3b6f61062c02
Lalit Maganti <lalitmaganti@gmail.com>
2014-08-14 23:07:48 +0100

    holoirc: properly send data through Crashlytics

--- android/src/internal/java/com/fusionx/lightirc/util/CrashUtils.java
+++ android/src/internal/java/com/fusionx/lightirc/util/CrashUtils.java
@@ -16,18 +16,26 @@ public class CrashUtils {
     }
 
     public static void logMissingData(final Server server) {
-        callbackIfReportingEnabled(() -> Crashlytics.log("Missing data on server "
-                + server.getConfiguration().getUrl()));
+        callbackIfReportingEnabled(
+                () -> Crashlytics.logException(new IRCException("Missing data on server "
+                        + server.getConfiguration().getUrl())));
     }
 
     public static void logIssue(final String data) {
-        callbackIfReportingEnabled(() -> Crashlytics.log(data));
+        callbackIfReportingEnabled(() -> Crashlytics.logException(new IRCException(data)));
     }
 
     private static void callbackIfReportingEnabled(final Runnable runnable) {
         final AppPreferences appPreferences = AppPreferences.getAppPreferences();
         if (appPreferences.isBugReportingEnabled()) {
             runnable.run();
+        }
+    }
+
+    private static class IRCException extends Exception {
+
+        public IRCException(final String data) {
+            super(data);
         }
     }
 }
