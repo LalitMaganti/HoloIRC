503da41f03f700cbed618e4a2e855cfd58adb517
Danny Baumann <dannybaumann@web.de>
2014-10-09 11:04:41 +0200

    Don't notify of closed PM channels.

--- android/src/main/java/com/fusionx/lightirc/util/EventUtils.java
+++ android/src/main/java/com/fusionx/lightirc/util/EventUtils.java
@@ -13,6 +13,8 @@ import co.fusionx.relay.event.Event;
 import co.fusionx.relay.event.channel.ChannelEvent;
 import co.fusionx.relay.event.channel.ChannelNameEvent;
 import co.fusionx.relay.event.channel.ChannelWorldUserEvent;
+import co.fusionx.relay.event.query.QueryClosedEvent;
+import co.fusionx.relay.event.query.QueryEvent;
 import co.fusionx.relay.event.server.JoinEvent;
 import co.fusionx.relay.event.server.NewPrivateMessageEvent;
 import co.fusionx.relay.event.server.ServerEvent;
@@ -29,6 +31,9 @@ public class EventUtils {
 
     private static final Set<Class<? extends DCCFileEvent>> DCC_FILE_IGNORE_EVENTS
             = ImmutableSet.of(DCCFileProgressEvent.class);
+
+    private static final Set<Class<? extends QueryEvent>> QUERY_IGNORE_EVENTS
+            = ImmutableSet.of(QueryClosedEvent.class);
 
     public static boolean shouldStoreEvent(final Event event) {
         if (event instanceof ChannelWorldUserEvent) {
@@ -47,6 +52,9 @@ public class EventUtils {
         } else if (event instanceof DCCFileEvent) {
             final DCCFileEvent fileEvent = (DCCFileEvent) event;
             return DCC_FILE_IGNORE_EVENTS.contains(fileEvent.getClass());
+        } else if (event instanceof QueryEvent) {
+            final QueryEvent queryEvent = (QueryEvent) event;
+            return !QUERY_IGNORE_EVENTS.contains(queryEvent.getClass());
         }
         return true;
     }
