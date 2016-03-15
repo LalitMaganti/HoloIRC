638caab05b56686e72fc0e262450296e72f2b8f8
Lalit Maganti <lalitmaganti@gmail.com>
2016-03-15 17:40:14 +0000

    gradle: switch to Fabric crashlytics and bump android gradle version

--- android/src/main/java/com/fusionx/lightirc/IRCApplication.java
+++ android/src/main/java/com/fusionx/lightirc/IRCApplication.java
@@ -3,6 +3,8 @@ package com.fusionx.lightirc;
 import android.app.Application;
 
 import com.fusionx.lightirc.misc.AppPreferences;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class IRCApplication extends Application {
     @Override
