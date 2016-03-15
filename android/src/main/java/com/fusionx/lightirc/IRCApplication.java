1f5bbbf16b5f4f42a7c64373661fc5f398429ac0
Lalit Maganti <lalitmaganti@gmail.com>
2016-03-15 19:46:16 +0000

    gradle: fix simple build

--- android/src/main/java/com/fusionx/lightirc/IRCApplication.java
+++ android/src/main/java/com/fusionx/lightirc/IRCApplication.java
@@ -3,8 +3,6 @@ package com.fusionx.lightirc;
 import android.app.Application;
 
 import com.fusionx.lightirc.misc.AppPreferences;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
 
 public class IRCApplication extends Application {
     @Override
